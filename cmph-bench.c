5ac8dac74e8b13b370747ea8a8a1604467a0194c
Jim Belton <jim.belton@gmail.com>
2017-06-25 12:13:34 -0700

    Added memory benchmarking

--- cmph-bench.c
+++ cmph-bench.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <malloc.h>
 #include <stdint.h>
 #include <string.h>
 #include <sys/types.h>
@@ -13,6 +14,13 @@ static uint64_t GetTimeStamp(void)
     struct timeval tv;
     gettimeofday(&tv,NULL);
     return tv.tv_sec * (uint64_t)1000000 + tv.tv_usec;
+}
+
+static size_t GetMemAlloced(void)
+{
+    struct mallinfo mallInfo;
+    mallInfo = mallinfo();
+    return mallInfo.uordblks;    /* Total allocated space (bytes) */
 }
 
 // Create minimal perfect hash function from in-memory vector
@@ -42,29 +50,36 @@ int main(int argc, char **argv)
     assert(corpusStream = fopen("corpus-words", "r"));
     cmph_io_adapter_t *source = cmph_io_nlfile_adapter(corpusStream);
 
-    // Creating a filled vector
+    // Open the dump file and get the time and memory stamps
     FILE *   mphf_fd = fopen("temp.mph", "w");
+    size_t   memBase = GetMemAlloced();
     uint64_t start   = GetTimeStamp();
 
-    //Create minimal perfect hash function using the brz algorithm.
+    //Create minimal perfect hash function using the chosen algorithm.
     cmph_config_t *config = cmph_config_new(source);
     cmph_config_set_algo(config, CMPH_BRZ);
     cmph_config_set_mphf_fd(config, mphf_fd);
     cmph_t *hash = cmph_new(config);
     cmph_config_destroy(config);
+    //cmph_io_vector_adapter_destroy(source);
+    uint64_t elapsed = GetTimeStamp()  - start;
+    size_t   memUsed = GetMemAlloced() - memBase;
+    printf("Constructed MPH in %lu.%06lu seconds, memory %lu\n", elapsed / 1000000, elapsed % 1000000, memUsed);
+
+    // Save the hash
     cmph_dump(hash, mphf_fd);
     cmph_destroy(hash);
     fclose(mphf_fd);
 
-    uint64_t elapsed = GetTimeStamp() - start;
-    printf("Constructed MPH in %lu.%lu seconds\n", elapsed / 1000000, elapsed % 1000000);
-
-    //Find key
+    // Benchmark the loader
     mphf_fd = fopen("temp.mph", "r");
     start   = GetTimeStamp();
+    memBase = GetMemAlloced();
     hash    = cmph_load(mphf_fd);
-    elapsed = GetTimeStamp() - start;
-    printf("Loaded MPH in %lu.%lu seconds\n", elapsed / 1000000, elapsed % 1000000);
+    fclose(mphf_fd);
+    elapsed = GetTimeStamp()  - start;
+    memUsed = GetMemAlloced() - memBase;
+    printf("Loaded MPH in %lu.%06lu seconds, memory %lu\n", elapsed / 1000000, elapsed % 1000000, memUsed);
 
     // Slurp in corpus
     struct stat corpusStat;
@@ -74,7 +89,8 @@ int main(int argc, char **argv)
     assert(corpusStream = fopen("corpus-words", "r"));
     assert(fread(corpus, 1, corpusStat.st_size, corpusStream) == corpusStat.st_size);
 
-    start = GetTimeStamp();
+    start   = GetTimeStamp();
+    memUsed = corpusStat.st_size;
     unsigned key;
 
     for (key = 0; corpusStat.st_size; key++) {
@@ -87,12 +103,12 @@ int main(int argc, char **argv)
         corpus              = end + 1;
     }
 
-    elapsed = GetTimeStamp() - start;
-    printf("Looked up %u keys in MPH in %lu.%lu seconds\n", key, elapsed / 1000000, elapsed % 1000000);
+    elapsed = GetTimeStamp()  - start;
+    printf("Looked up %u keys in MPH in %lu.%06lu seconds, corpus size %lu\n",
+           key, elapsed / 1000000, elapsed % 1000000, memUsed);
 
     //Destroy hash
     cmph_destroy(hash);
-    cmph_io_vector_adapter_destroy(source);
-    fclose(mphf_fd);
+    cmph_io_vector_adapter_destroy(source);    // Must be here. Moving it up to where it's commented out makes exit hang
     return 0;
 }
