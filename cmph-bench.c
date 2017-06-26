a0009d248371bdf72f0ace5a19626d0f51180c78
Jim Belton <jim.belton@gmail.com>
2017-06-25 18:30:15 -0700

    Respect users chosen algo

--- cmph-bench.c
+++ cmph-bench.c
@@ -57,7 +57,7 @@ int main(int argc, char **argv)
 
     //Create minimal perfect hash function using the chosen algorithm.
     cmph_config_t *config = cmph_config_new(source);
-    cmph_config_set_algo(config, CMPH_BRZ);
+    cmph_config_set_algo(config, algo);
     cmph_config_set_mphf_fd(config, mphf_fd);
     cmph_t *hash = cmph_new(config);
     cmph_config_destroy(config);
